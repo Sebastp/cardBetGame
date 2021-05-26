6ef070b2344fe017a1f8f4018dbb5740bec5e320
Sebastian Pobocha <sebastianpob@gmail.com>
2021-05-26 19:01:58 +0200

    Ui code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,39 @@ import React, { useState, useEffect } from 'react'
 import Nav from '@views/components/Nav'
 
 const Home = () => {
-  const [desckId, desckIdSet] = useState('')
+  const [desckId, desckIdSet] = useState(''),
+    [currentCard, currentCardSet] = useState({ image: '', code: '' }),
+    [remainingCards, remainingCardsSet] = useState(1),
+    [userBet, userBetSet] = useState<'string' | void>(null)
+
   const shuffleCards = () => {
-    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', {
-      method: 'GET',
-    })
-      .then((data) => {
-        console.log('Success:', data)
-        // desckIdSet
+    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:1')
+        desckIdSet(data.deck_id)
+        drawCard(data.deck_id)
       })
-      .catch((error) => {
+      .catch(error => {
         alert('Something went wrong!')
         console.error('Error:', error)
       })
   }
-  const drawCard = () => {
-    fetch(`https://deckofcardsapi.com/api/deck/${desckId}/draw/?count=1`, {
+  const drawCard = id => {
+    if (remainingCards < 1) {
+      shuffleCards()
+      return
+    }
+    fetch(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=1`, {
       method: 'GET',
     })
-      .then((data) => {
-        console.log('Success:', data)
+      .then(response => response.json())
+      .then(data => {
+        currentCardSet(data.cards[0])
+        remainingCardsSet(data.remaining)
+        console.log('Success:2')
       })
-      .catch((error) => {
+      .catch(error => {
         alert('Something went wrong!')
         console.error('Error:', error)
       })
@@ -32,12 +43,25 @@ const Home = () => {
   useEffect(() => {
     shuffleCards()
   }, [])
+
   return (
-    <div>
+    <div className="cardPage">
       <section></section>
       <section className="callToActions">
-        <button>Up</button>
-        <button>Down</button>
+        <button
+          onClick={() => {
+            userBetSet('up')
+          }}
+        >
+          Up
+        </button>
+        <button
+          onClick={() => {
+            userBetSet('down')
+          }}
+        >
+          Down
+        </button>
       </section>
     </div>
   )
